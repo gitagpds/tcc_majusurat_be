e4937410f2f77269a5b3e7e7e0f4d0355e3191c4
Gita Poetri Dewi Siregar <146951904+gitagpds@users.noreply.github.com>
2025-06-11 21:58:09 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("majusurat_db", "root", "", {
-    host: "localhost",
+const db = new Sequelize("majusurat_db", "root", "majusurat123", {
+    host: "34.45.107.245",
     dialect: "mysql"
 
 })
