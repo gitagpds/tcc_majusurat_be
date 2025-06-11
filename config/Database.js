05c94a2982d5dd670757a3f97d843f8a8fe2100a
Gita Poetri Dewi Siregar <146951904+gitagpds@users.noreply.github.com>
2025-06-11 21:37:08 +0700

    Update Database.js

--- config/Database.js
+++ config/Database.js
@@ -1,14 +1,9 @@
 import { Sequelize } from "sequelize";
-import "dotenv/config";
 
-const DB_NAME = process.env.DB_NAME;
-const DB_USERNAME = process.env.DB_USERNAME;
-const DB_PASSWORD = process.env.DB_PASSWORD;
-const DB_HOST = process.env.DB_HOST;
+const db = new Sequelize("majusurat_db", "root", "", {
+    host: "localhost",
+    dialect: "mysql"
 
-const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
-  host: DB_HOST,
-  dialect: "mysql",
-});
+})
 
 export default db;
